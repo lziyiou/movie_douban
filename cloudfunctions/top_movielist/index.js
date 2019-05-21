e86d91e9ea6e7e31b4dc62a94600d041c60d9e3b
L-ziyiou <787492833@qq.com>
2019-05-21 12:16:43 +0800

    Initial Commit

---
+++ cloudfunctions/top_movielist/index.js
@@ -0,0 +1,18 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+
+var rp = require('request-promise');
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+
+  return rp(`http://api.douban.com/v2/movie/top250?apikey=0df993c66c0c636e29ecbb5344252a4a&start=${event.start}&count=${event.count}`)
+    .then(function (htmlString) {
+      return htmlString
+    })
+    .catch(function (err) {
+      console.err(err)
+    });
+}
---
+++ miniprogram/pages/rating/rating.js
@@ -0,0 +1,88 @@
+// pages/rating/rating.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    movieList: []
+  },
+
+  getMovieList: function(){
+    wx.showLoading({
+      title: '加载中',
+    })
+    wx.cloud.callFunction({
+      name: "top_movielist",
+      data: {
+        start: this.data.movieList.length,
+        count: 5
+      }
+    }).then(res => {
+      this.setData({
+        movieList: this.data.movieList.concat(JSON.parse(res.result).subjects)
+      })
+      console.log(res)
+      wx.hideLoading();
+    }).catch(err => {
+      wx.hideLoading()
+      console.error(err);
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.getMovieList();
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    this.getMovieList();
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
